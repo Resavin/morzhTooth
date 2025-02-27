1537843b3c6da59a7fffaba3b50fb364ccd7dded
Resavin <rairlord@gmail.com>
2025-02-28 00:10:43 +0300

    add mongo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/morzhdb";
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Bun + Express + TypeScript!");
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Bun + Express + MongoDB!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
